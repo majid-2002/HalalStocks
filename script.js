18ffb14be452e01b4018527c1cd157f8ad78fee9
Abdul Majid <majid616365@gmail.com>
2023-11-08 14:13:21 +0530

    update css

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ const getStocksToWatch = async () => {
     const aTag = doc.querySelector(
       'a.smallcard-title[href*="stocks-to-watch"]'
     );
+    const h2Tag = document.querySelector("#root h2"); // Target the <h2> tag in your HTML
 
     if (aTag) {
-      console.log(aTag.outerHTML);
+      h2Tag.textContent = aTag.outerHTML; // Update the <h2> content with the <a> tag's text
     } else {
-      console.log("Anchor tag not found.");
+      h2Tag.textContent = "Anchor tag not found."; // Update <h2> if the anchor tag is not found
     }
   } else {
     console.error("Failed to fetch data");
